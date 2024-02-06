078158e96f260570ddf1f3c47c6a37e540399e3c
haripriyaro <haripriya@onetera.com>
2024-02-06 13:58:39 -0800

    UI of login and signup page completed

---
+++ frontend/pages/login/index.tsx
@@ -0,0 +1,53 @@
+import { Form, Input, Button, Checkbox } from 'antd';
+import Link from 'next/link';
+
+const LoginPage = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div style={{ maxWidth: '300px', margin: 'auto', marginTop: '50px' }}>
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+      <Link href="/signup">Don't have an account? Sign up</Link>
+    </div>
+  );
+};
+
+export default LoginPage;
