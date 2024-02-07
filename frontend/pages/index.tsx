c2743e7d1a94ac62a7802ca1d90ff64962b5b2e9
haripriyaro <haripriya@onetera.com>
2024-02-07 09:43:48 -0800

    modified page name in the main landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,14 @@
+import Head from "next/head";
 import Navbar from "../components/Navbar/Navbar";
-
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      <Head>
+        <title>Onetera Techbologies</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Navbar />
       <h1>Welcome to Onetera!</h1>
     </div>
   );
